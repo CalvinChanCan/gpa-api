91a4639f186497d62824ebda099cf306fb37d11f
Calvin <calvinchancan@gmail.com>
2023-03-06 01:35:52 -0500

    Fix cors issue by moving cors middleware to top

--- api/settings.py
+++ api/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -53,7 +54,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 CORS_ORIGIN_WHITELIST = [
@@ -62,6 +62,7 @@ CORS_ORIGIN_WHITELIST = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+APPEND_SLASH = False
 
 
 ROOT_URLCONF = "api.urls"
